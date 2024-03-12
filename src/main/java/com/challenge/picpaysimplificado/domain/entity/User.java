5037d1f183c9afe8b511f544bc3fb874ebecd95a
RaulMafra <raulcesar.sm@gmail.com>
2024-03-12 14:22:03 -0300

    feat: inserido atributos que nao podem ser nulos

--- src/main/java/com/challenge/picpaysimplificado/domain/entity/User.java
+++ src/main/java/com/challenge/picpaysimplificado/domain/entity/User.java
@@ -1,13 +1,14 @@
 package com.challenge.picpaysimplificado.domain.entity;
 
 import com.challenge.picpaysimplificado.domain.entity.enumerator.UserType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.challenge.picpaysimplificado.dto.request.CreateUserDTO;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "users")
@@ -17,12 +18,27 @@ import lombok.Setter;
 @NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_user")
     private Long id;
+    @Column(nullable = false)
     private String name;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String document;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
+    private BigDecimal balance;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(CreateUserDTO createUserDTO){
+        this.name = createUserDTO.name();
+        this.document = createUserDTO.document();
+        this.email = createUserDTO.email();
+        this.balance = createUserDTO.balance();
+        this.userType = createUserDTO.userType();
+    }
 }
