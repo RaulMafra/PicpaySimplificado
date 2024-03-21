6f707b6f4adba344a8bcf196283f54e0d5525e8c
RaulMafra <raulcesar.sm@gmail.com>
2024-03-21 14:41:32 -0300

    refactor: alterado o nome do atributo value para amount

--- src/main/java/com/challenge/picpaysimplificado/dto/request/TransactionDTO.java
+++ src/main/java/com/challenge/picpaysimplificado/dto/request/TransactionDTO.java
@@ -2,5 +2,5 @@ package com.challenge.picpaysimplificado.dto.request;
 
 import java.math.BigDecimal;
 
-public record TransactionDTO(BigDecimal value, Long payer, Long receiver) {
+public record TransactionDTO(BigDecimal amount, Long payer, Long receiver) {
 }
