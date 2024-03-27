c63d71da9f7299f9a7c8e1f149f42238cb77ee4a
RaulMafra <raulcesar.sm@gmail.com>
2024-03-27 19:19:25 -0300

    refactor: refatoração no serviço de notificação

--- src/main/java/com/challenge/picpaysimplificado/webservices/SendNotification.java
+++ src/main/java/com/challenge/picpaysimplificado/webservices/SendNotification.java
@@ -1,9 +1,13 @@
 package com.challenge.picpaysimplificado.webservices;
 
+import com.challenge.picpaysimplificado.domain.User;
+import com.challenge.picpaysimplificado.dto.request.NotificationDTO;
 import com.challenge.picpaysimplificado.exceptionshandler.exceptions.WebServiceException;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -18,11 +22,15 @@ public class SendNotification {
     @Value("${email.service.url}")
     private String url;
 
-    public void send(){
-        try{
-            this.restTemplate.getForEntity(url, String.class);
+    public void send(User user, String message) {
+        NotificationDTO notificationDTO = new NotificationDTO(user.getEmail(), message);
+        ResponseEntity<String> notificationResponse;
+        try {
+            notificationResponse = this.restTemplate.postForEntity(url, notificationDTO, String.class);
+        } catch (RestClientException e) {
+            throw new WebServiceException("Email service is unavailable");
         }
-        catch (RestClientException e){
+        if (notificationResponse.getStatusCode() != HttpStatus.OK) {
             throw new WebServiceException("Email service is unavailable");
         }
     }
