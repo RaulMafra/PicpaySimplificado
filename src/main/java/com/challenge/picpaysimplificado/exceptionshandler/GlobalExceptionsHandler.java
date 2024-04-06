fad90bc66e68c30c62ed0de408c93a07f0911702
RaulMafra <raulcesar.sm@gmail.com>
2024-04-06 20:43:49 -0300

    refactor: Alterado o nome e tipo da classe HttpResponseBody.java para ResponseErrorDTO.java

--- src/main/java/com/challenge/picpaysimplificado/exceptionshandler/GlobalExceptionsHandler.java
+++ src/main/java/com/challenge/picpaysimplificado/exceptionshandler/GlobalExceptionsHandler.java
@@ -1,5 +1,6 @@
 package com.challenge.picpaysimplificado.exceptionshandler;
 
+import com.challenge.picpaysimplificado.dto.response.ResponseErrorDTO;
 import com.challenge.picpaysimplificado.exceptionshandler.exceptions.TransactionException;
 import com.challenge.picpaysimplificado.exceptionshandler.exceptions.UserException;
 import com.challenge.picpaysimplificado.exceptionshandler.exceptions.UserNotFound;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionsHandler {
 
@@ -24,40 +27,40 @@ public class GlobalExceptionsHandler {
     }
 
     @ExceptionHandler(TransactionException.class)
-    private ResponseEntity<HttpResponseBody> transactionException(TransactionException e, WebRequest request, HttpServletRequest httpServletRequest){
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+    private ResponseEntity<ResponseErrorDTO> transactionException(TransactionException e, WebRequest request, HttpServletRequest httpServletRequest){
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
     @ExceptionHandler(UserNotFound.class)
-    private ResponseEntity<HttpResponseBody> transactionException(UserNotFound e, WebRequest request, HttpServletRequest httpServletRequest){
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, e.getMessage(), httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+    private ResponseEntity<ResponseErrorDTO> transactionException(UserNotFound e, WebRequest request, HttpServletRequest httpServletRequest){
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, e.getMessage(), httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
     @ExceptionHandler(UserException.class)
-    private ResponseEntity<HttpResponseBody> transactionException(UserException e, WebRequest request, HttpServletRequest httpServletRequest){
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+    private ResponseEntity<ResponseErrorDTO> transactionException(UserException e, WebRequest request, HttpServletRequest httpServletRequest){
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST, e.getMessage(), httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
     @ExceptionHandler(WebServiceException.class)
-    private ResponseEntity<HttpResponseBody> transactionException(WebServiceException e, WebRequest request, HttpServletRequest httpServletRequest){
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.SERVICE_UNAVAILABLE.value(), HttpStatus.SERVICE_UNAVAILABLE, e.getMessage(), httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+    private ResponseEntity<ResponseErrorDTO> transactionException(WebServiceException e, WebRequest request, HttpServletRequest httpServletRequest){
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.SERVICE_UNAVAILABLE.value(), HttpStatus.SERVICE_UNAVAILABLE, e.getMessage(), httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    private ResponseEntity<HttpResponseBody> handleDataPersistence(DataIntegrityViolationException e, WebRequest request, HttpServletRequest httpServletRequest){
+    private ResponseEntity<ResponseErrorDTO> handleDataPersistence(DataIntegrityViolationException e, WebRequest request, HttpServletRequest httpServletRequest){
         String response = "Conflict when persisting";
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, response, httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT, response, httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
     @ExceptionHandler(RuntimeException.class)
-    private ResponseEntity<HttpResponseBody> transactionException(RuntimeException e, WebRequest request, HttpServletRequest httpServletRequest){
-        HttpResponseBody httpResponseBody = new HttpResponseBody(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), httpServletRequest.getRequestURI());
-        return new ResponseEntity<>(httpResponseBody, headers(), httpResponseBody.getStatus());
+    private ResponseEntity<ResponseErrorDTO> transactionException(RuntimeException e, WebRequest request, HttpServletRequest httpServletRequest){
+        ResponseErrorDTO responseError = new ResponseErrorDTO(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), httpServletRequest.getRequestURI());
+        return new ResponseEntity<>(responseError, headers(), responseError.status());
     }
 
 }
