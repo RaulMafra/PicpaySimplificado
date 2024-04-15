539a5581a8eb0bda6d46d789bb57a0fa89edbc22
RaulMafra <raulcesar.sm@gmail.com>
2024-04-15 01:15:25 -0300

    feat: Adicionado busca do objeto User pelo document no database

--- src/main/java/com/challenge/picpaysimplificado/repository/UserRepository.java
+++ src/main/java/com/challenge/picpaysimplificado/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.challenge.picpaysimplificado.repository;
 import com.challenge.picpaysimplificado.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByDocument(String document);
 }
